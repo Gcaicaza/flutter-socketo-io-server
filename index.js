425306e7db269b2e693f25b6c0fea2551172069e
Icaza_Carlos <caicaza@uce.edu.ec>
2021-07-19 19:48:09 -0500

    Fin sección 3

---
+++ index.js
@@ -0,0 +1,26 @@
+//levantando servidor 
+const express = require('express');
+const app = express();
+
+const path = require('path');
+require('dotenv').config();
+
+//servidor de sockets
+//Node SERVER
+
+///////socket io para backends. Socket es como un cliente conectado
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+require('./sockets/socket');
+
+
+//path publico
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err)=>{
+  if(err) throw new Error(err);
+
+  console.log('Servidor corriendo en puerto', process.env.PORT);
+});
